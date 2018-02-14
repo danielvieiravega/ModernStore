8f0f45b4c72dd44895e9c073033cdcbb17fdf9a6
Daniel Vieira Vega <daniel.vega@acad.pucrs.br>
2018-02-14 12:43:55 -0200

    Finalizada a aula 14.

---
+++ ModernStore.Infra/Respositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using ModernStore.Domain.Repositories;
+using ModernStore.Domain.Entities;
+using ModernStore.Infra.Contexts;
+
+namespace ModernStore.Infra.Respositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ModernStoreDataContext _context;
+
+        public OrderRepository(ModernStoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Save(Order order)
+        {
+            _context.Orders.Add(order);
+        }
+    }
+}
